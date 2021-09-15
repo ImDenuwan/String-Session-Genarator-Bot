f2f018b625c2677145f6dd38d5e12a7431241c52
ImDenuwan <90548223+ImDenuwan@users.noreply.github.com>
2021-09-15 20:06:35 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# TG String Session
+Generate Pyrogram String Session Using this bot.
+
+## Demo:
+<a href="https://telegram.dog/SZGenStrBot"><img src="https://img.shields.io/badge/Telegram-Bot-blue.svg?logo=telegram"></a>
+
+## Configs:
+- API_HASH
+  - Get from [Here](https://my.telegram.org).
+- API_ID
+  - Get from [Here](https://my.telegram.org).
+- BOT_TOKEN
+  - Telegram Bot Token from [here](https://telegram.dog/BotFather).
+
+## Deploy Now:
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/ImDenuwan/SZ-Gen-Str-Bot)
+
+## Support Group:
+<a href="https://telegram.dog/slbotzone"><img src="https://img.shields.io/badge/Telegram-Join%20Telegram%20Group-blue.svg?logo=telegram"></a>
+
---
+++ plugins/genStr.py
@@ -0,0 +1,266 @@
+import os
+import json
+import time
+import asyncio
+
+from asyncio.exceptions import TimeoutError
+
+from pyrogram import filters, Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import (
+    SessionPasswordNeeded, FloodWait,
+    PhoneNumberInvalid, ApiIdInvalid,
+    PhoneCodeInvalid, PhoneCodeExpired
+)
+
+
+API_TEXT = """🙋‍♂ Hi {},
+
+I am a Pyrogram String Session generator bot.
+
+⚠️ Disclaimer - This bot is 100% safe. But please make sure that you properly know what a string session is & how it performs, brfore using me.😍
+
+Now send your `API_ID` same as `APP_ID` to Start Generating Session.
+
+Project of @slbotzone
+"""
+HASH_TEXT = "Ok Now Send your `API_HASH` to Continue.\n\nPress /cancel to Cancel.🐧"
+PHONE_NUMBER_TEXT = (
+    "📞__ Now send your Phone number to Continue"
+    " include Country code.__\n**Eg:** `+94771234567`\n\n"
+    "Press /cancel to Cancel."
+)
+buttons = [
+    [
+    InlineKeyboardButton(
+        "📃 More Bots", url="https://t.me/szbots/8"),
+    InlineKeyboardButton(
+        "💭 Support Group", url="https://t.me/slbotzone")
+        ]]
+                       
+
+
+@Client.on_message(filters.private & filters.command("start"))
+async def generate_str(c, m):
+    get_api_id = await c.ask(
+        chat_id=m.chat.id,
+        text=API_TEXT.format(m.from_user.mention(style='md')),
+        filters=filters.text,
+        reply_markup = InlineKeyboardMarkup(buttons)
+    )
+    api_id = get_api_id.text
+    if await is_cancel(m, api_id):
+        return
+    try:
+        check_api = int(api_id)
+    except Exception:
+        await m.reply("**--🛑 API ID Invalid 🛑--**\nPress /start to create again.")
+        return
+
+    get_api_hash = await c.ask(
+        chat_id=m.chat.id, 
+        text=HASH_TEXT,
+        filters=filters.text
+    )
+    api_hash = get_api_hash.text
+    if await is_cancel(m, api_hash):
+        return
+    if not len(api_hash) >= 30:
+        await m.reply("--**🛑 API HASH Invalid 🛑**--\nPress /start to create again.")
+        return
+
+    try:
+        client = Client(":memory:", api_id=api_id, api_hash=api_hash)
+    except Exception as e:
+        await c.send_message(m.chat.id ,f"**🛑 ERROR: 🛑** `{str(e)}`\nPress /start to create again.")
+        return
+
+    try:
+        await client.connect()
+    except ConnectionError:
+        await client.disconnect()
+        await client.connect()
+    while True:
+        get_phone_number = await c.ask(
+            chat_id=m.chat.id,
+            text=PHONE_NUMBER_TEXT
+        )
+        phone_number = get_phone_number.text
+        if await is_cancel(m, phone_number):
+            return
+
+        confirm = await c.ask(
+            chat_id=m.chat.id,
+            text=f'🤔 Is `{phone_number}` correct? (y/n): \n\ntype: `y` (If Yes)\ntype: `n` (If No)'
+        )
+        if await is_cancel(m, confirm.text):
+            return
+        if "y" in confirm.text.lower():
+            break
+    try:
+        code = await client.send_code(phone_number)
+        await asyncio.sleep(1)
+    except FloodWait as e:
+        await m.reply(f"__Sorry to say you that you have floodwait of {e.x} Seconds 😞__")
+        return
+    except ApiIdInvalid:
+        await m.reply("🕵‍♂ The API ID or API HASH is Invalid.\n\nPress /start to create again.")
+        return
+    except PhoneNumberInvalid:
+        await m.reply("☎ Your Phone Number is Invalid.`\n\nPress /start to create again.")
+        return
+
+    try:
+        sent_type = {"app": "Telegram App 💌",
+            "sms": "SMS 💬",
+            "call": "Phone call 📱",
+            "flash_call": "phone flash call 📲"
+        }[code.type]
+        otp = await c.ask(
+            chat_id=m.chat.id,
+            text=(f"I had sent an OTP to the number `{phone_number}` through {sent_type}\n\n"
+                  "Please enter the OTP in the format `1 2 3 4 5` __(provied white space between numbers)__\n\n"
+                  "If Bot not sending OTP then try /start the Bot.\n"
+                  "Press /cancel to Cancel."), timeout=300)
+    except TimeoutError:
+        await m.reply("**⏰ TimeOut Error:** You reached Time limit of 5 min.\nPress /start to create again.")
+        return
+    if await is_cancel(m, otp.text):
+        return
+    otp_code = otp.text
+    try:
+        await client.sign_in(phone_number, code.phone_code_hash, phone_code=' '.join(str(otp_code)))
+    except PhoneCodeInvalid:
+        await m.reply("**📵 Invalid Code**\n\nPress /start to create again.")
+        return 
+    except PhoneCodeExpired:
+        await m.reply("**⌚ Code is Expired**\n\nPress /start to create again.")
+        return
+    except SessionPasswordNeeded:
+        try:
+            two_step_code = await c.ask(
+                chat_id=m.chat.id, 
+                text="`🔐 This account have two-step verification code.\nPlease enter your second factor authentication code.`\nPress /cancel to Cancel.",
+                timeout=300
+            )
+        except TimeoutError:
+            await m.reply("**⏰ TimeOut Error:** You reached Time limit of 5 min.\nPress /start to create again.")
+            return
+        if await is_cancel(m, two_step_code.text):
+            return
+        new_code = two_step_code.text
+        await two_step_code.delete()
+        await two_step_code.request.delete()
+        try:
+            await client.check_password(new_code)
+        except Exception as e:
+            await m.reply(f"**⚠️ ERROR:** `{str(e)}`")
+            return
+    except Exception as e:
+        await c.send_message(m.chat.id ,f"**⚠️ ERROR:** `{str(e)}`")
+        return
+    try:
+        session_string = await client.export_session_string()
+        await client.send_message("me", f"**Your String Session 👇**\n\n`{session_string}`\n\nThanks For using {(await c.get_me()).mention(style='md')}")
+        text = "✅ Congratulations! Successfully Generated Your Pyrogram String Session and sent to you saved messages.\nCheck your saved messages or Click on Below Button."
+        reply_markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="String Session ↗️", url=f"tg://openmessage?user_id={m.chat.id}")]]
+        )
+        await c.send_message(m.chat.id, text, reply_markup=reply_markup)
+    except Exception as e:
+        await c.send_message(m.chat.id ,f"**⚠️ ERROR:** `{str(e)}`")
+        return
+    try:
+        await client.stop()
+    except:
+        pass
+
+
+@Client.on_message(filters.private & filters.command("help"))
+async def help(c, m):
+    await help_cb(c, m, cb=False)
+
+
+@Client.on_callback_query(filters.regex('^help$'))
+async def help_cb(c, m, cb=True):
+    help_text = """**Hey You need Help??👨‍✈️**
+
+
+>>>> Press the start button
+
+>>>> Send Your API_ID when bot ask.
+
+>>>> Then send your API_HASH when bot ask.
+
+>>>> Send your mobile number.
+
+>>>> Send the OTP reciveved to your numer in the format `1 2 3 4 5` (Give space b/w each digit)
+
+>>>> (If you have two step verification send to bot if bot ask.)
+
+
+**NOTE:**
+
+If bot not Sending OTP to your Phone Number than send /cancle Command and again send /start to Start your Process. 
+
+Project of @slbotzone.
+"""
+
+    buttons = [[
+        InlineKeyboardButton('📕 About', callback_data='about'),
+        InlineKeyboardButton('❌ Close', callback_data='close')
+    ]]
+    if cb:
+        await m.answer()
+        await m.message.edit(text=help_text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    else:
+        await m.reply_text(text=help_text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True, quote=True)
+
+
+@Client.on_message(filters.private & filters.command("about"))
+async def about(c, m):
+    await about_cb(c, m, cb=False)
+
+
+@Client.on_callback_query(filters.regex('^about$'))
+async def about_cb(c, m, cb=True):
+    me = await c.get_me()
+    about_text = f"""**MY DETAILS:**
+
+__🤖 My Name:__ {me.mention(style='md')}
+    
+__📝 Language:__ [Python3](https://www.python.org/)
+
+__🧰 Framework:__ [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+__📃 More bots:__ [TG Bots](https://t.me/szbots/8)
+
+__📢 Channel:__ [BOT UPDATES](https://t.me/szbots)
+
+__👥 Group:__ [BOT SUPPORT](https://t.me/slbotzone)
+"""
+
+    buttons = [[
+        InlineKeyboardButton('💡 Help', callback_data='help'),
+        InlineKeyboardButton('❌ Close', callback_data='close')
+    ]]
+    if cb:
+        await m.answer()
+        await m.message.edit(about_text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    else:
+        await m.reply_text(about_text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True, quote=True)
+
+
+@Client.on_callback_query(filters.regex('^close$'))
+async def close(c, m):
+    await m.message.delete()
+    await m.message.reply_to_message.delete()
+
+
+async def is_cancel(msg: Message, text: str):
+    if text.startswith("/cancel"):
+        await msg.reply("⛔ Process Cancelled.")
+        return True
+    return False
+
+
